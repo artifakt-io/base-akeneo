77cafeec4c34276c4067c6d96ffbc11de932b0e9
djalal <djalal@users.noreply.github.com>
2021-05-25 23:11:44 +0200

    enable container integration (#6)
    * enable container integration
    
    * fix failing crontab

---
+++ .artifakt/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo ">>>>>>>>>>>>>> START CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
+
+echo "------------------------------------------------------------"
+echo "The following build args are available:"
+env
+echo "------------------------------------------------------------"
+
+su www-data -s /bin/sh -c 'composer install && composer dump-autoload'
+
+chmod 755 /var/www/html/pim-community-standard/bin/console
+
+#chown -R www-data:www-data /var/www/html/pim-community-standard/vendor
+
+echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
---
+++ .artifakt/init-host.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# on starter only
+# ignore on pro/entreprise
+
+ID=$(sudo docker ps -ql)
+
+#TODO STEP1 run elasticsearch
+sudo docker cp ${ID}:/.artifakt/docker-compose.yaml /tmp/docker-compose.yaml
+
+sudo docker-compose --file=/tmp/docker-compose.yaml --project-name=app up --remove-orphans -d

740d64c203bf89fff741c33df4b21a42cb499218
djalal <djalal@users.noreply.github.com>
2021-06-16 22:02:40 +0200

    remove .artifakt/init-host.sh

--- .artifakt/init-host.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo ">>>>>>>>>>>>>> START AKENEO INIT-HOST SCRIPT <<<<<<<<<<<<<<<<< "
-env
-sudo docker ps -a
-echo ">>>>>>>>>>>>>> END AKENEO INIT-HOST SCRIPT <<<<<<<<<<<<<<<<< "

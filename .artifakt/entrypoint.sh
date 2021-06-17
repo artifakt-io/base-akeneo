3716b0382dc2ea43b72d6c75201e16723348b124
djalal <djalal@users.noreply.github.com>
2021-06-17 13:33:00 +0200

    [ch5508] clean slow composer in entrypoint
    Signed-off-by: djalal <djalal@users.noreply.github.com>

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -18,8 +18,6 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
-su www-data -s /bin/sh -c 'composer install && composer dump-autoload'
-
 wait-for-it.sh $APP_DATABASE_HOST:3306 --timeout=90 -- su www-data -s /bin/sh -c 'sleep 10 && cd /var/www/html/pim-community-standard && APP_ENV=dev php bin/console pim:installer:db --catalog vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal || :'
 
 su www-data -s /bin/sh -c 'php ./bin/console pim:user:create kbeck secretp@ssw0rd kbeck@example.com Kent Beck en_US --admin -n'

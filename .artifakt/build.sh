f22d9334b1b620f7978ad42cabb5b993a5674fd6
djalal <djalal@users.noreply.github.com>
2021-07-09 15:21:39 +0200

    fix local build

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -9,11 +9,8 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
-su www-data -s /bin/sh -c 'composer install && composer dump-autoload'
+su www-data -s /bin/sh -c ' composer install --no-cache --optimize-autoloader --no-interaction --no-ansi && composer dump-autoload'
 
 chmod 755 /var/www/html/pim-community-standard/bin/console
-
-#CLEAN
-#chown -R www-data:www-data /var/www/html/pim-community-standard/vendor
 
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "

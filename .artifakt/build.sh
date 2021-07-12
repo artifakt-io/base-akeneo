95a51238c6b9990da1dd8bf7777332bf8a3f9b0d
djalal <djalal@users.noreply.github.com>
2021-07-12 10:25:30 +0200

    akeneo5 version bump and local build (#17)
    * add logs as a volume, production like

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -9,7 +9,7 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
-su www-data -s /bin/sh -c 'composer install && composer dump-autoload'
+su www-data -s /bin/sh -c ' composer install --no-cache --optimize-autoloader --no-interaction --no-ansi && composer dump-autoload'
 
 chmod 755 /var/www/html/pim-community-standard/bin/console
 
--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -18,6 +18,8 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
+wait-for-it.sh ${ARTIFAKT_ES_HOST:-elasticsearch}:${ARTIFAKT_ES_PORT:-9200} --timeout=30
+
 wait-for-it.sh $APP_DATABASE_HOST:3306 --timeout=90 -- su www-data -s /bin/sh -c 'sleep 10 && cd /var/www/html/pim-community-standard && APP_ENV=dev php bin/console pim:installer:db --catalog vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal || :'
 
 su www-data -s /bin/sh -c 'php ./bin/console pim:user:create kbeck secretp@ssw0rd kbeck@example.com Kent Beck en_US --admin -n'

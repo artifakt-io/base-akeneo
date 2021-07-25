7ad22c6fba7affcb0f62bbae207833fe76f8d9d0
djalal <djalal@users.noreply.github.com>
2021-07-25 20:48:58 +0200

    remove env block

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -13,11 +13,6 @@ export APP_DATABASE_HOST=${ARTIFAKT_MYSQL_HOST:-mysql}
 export APP_DATABASE_PORT=${ARTIFAKT_MYSQL_PORT:-3306}
 export APP_INDEX_HOSTS='elasticsearch:9200'
 
-echo "------------------------------------------------------------"
-echo "The following build args are available:"
-env
-echo "------------------------------------------------------------"
-
 wait-for-it.sh ${ARTIFAKT_ES_HOST:-elasticsearch}:${ARTIFAKT_ES_PORT:-9200} --timeout=30
 
 wait-for-it.sh $APP_DATABASE_HOST:3306 --timeout=90 -- su www-data -s /bin/sh -c 'sleep 10 && cd /var/www/html/pim-community-standard && APP_ENV=dev php bin/console pim:installer:db --catalog vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal || :'

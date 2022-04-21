19b51de1600902de6d1ddcc487a73374a8671d1a
djalal <djalal@users.noreply.github.com>
2022-04-21 19:45:18 +0200

    persist env vars for entrypoint usage

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -9,8 +9,10 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
-su www-data -s /bin/sh -c ' composer install --no-cache --optimize-autoloader --no-interaction --no-ansi && composer dump-autoload'
+su www-data -s /bin/sh -c 'composer install --no-cache --optimize-autoloader --no-interaction --no-ansi && composer dump-autoload'
+su www-data -s /bin/sh -c 'cp /tmp/build-args /var/www/html/.build-args'
 
 chmod 755 /var/www/html/pim-community-standard/bin/console
+chmod 755 /var/www/html/.build-args
 
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "

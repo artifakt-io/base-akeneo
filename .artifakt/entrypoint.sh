724abc9711c3ed836b80e3544a1c7de3e5afdffa
djalal <djalal@users.noreply.github.com>
2022-04-26 09:22:33 +0200

    init env. vars on the fly

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -6,12 +6,6 @@ echo ">>>>>>>>>>>>>> START CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
 
 # set runtime env. vars on the fly
 source /var/www/html/.build-args
-export APP_ENV=prod
-export APP_DATABASE_NAME=${ARTIFAKT_MYSQL_DATABASE_NAME:-changeme}
-export APP_DATABASE_USER=${ARTIFAKT_MYSQL_USER:-changeme}
-export APP_DATABASE_PASSWORD=${ARTIFAKT_MYSQL_PASSWORD:-changeme}
-export APP_DATABASE_HOST=${ARTIFAKT_MYSQL_HOST:-mysql}
-export APP_DATABASE_PORT=${ARTIFAKT_MYSQL_PORT:-3306}
 
 ES_PROTOCOL=""
 if [[ "$ARTIFAKT_ES_PORT" == "443" ]]; then
@@ -23,6 +17,21 @@ wait-for ${ARTIFAKT_ES_HOST:-elasticsearch}:${ARTIFAKT_ES_PORT:-9200} --timeout=
 
 wait-for $APP_DATABASE_HOST:3306 --timeout=90 -- su www-data -s /bin/bash -c '
   cd /var/www/html/pim-community-standard
+  source /var/www/html/.build-args
+
+  export APP_ENV=prod
+  export APP_DATABASE_NAME=${ARTIFAKT_MYSQL_DATABASE_NAME:-changeme}
+  export APP_DATABASE_USER=${ARTIFAKT_MYSQL_USER:-changeme}
+  export APP_DATABASE_PASSWORD=${ARTIFAKT_MYSQL_PASSWORD:-changeme}
+  export APP_DATABASE_HOST=${ARTIFAKT_MYSQL_HOST:-mysql}
+  export APP_DATABASE_PORT=${ARTIFAKT_MYSQL_PORT:-3306}
+
+  ES_PROTOCOL=""
+  if [[ "$ARTIFAKT_ES_PORT" == "443" ]]; then
+    ES_PROTOCOL="https://"
+  fi
+  export APP_INDEX_HOSTS=${ES_PROTOCOL:-http://}${ARTIFAKT_ES_HOST:-elasticsearch}:${ARTIFAKT_ES_PORT:-9200}
+  
   ./bin/console pim:system:information 2>/dev/null;
 
   if [ $? -ne 0 ]; then

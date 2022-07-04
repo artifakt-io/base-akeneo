279f1c7ac84e9d8a14744c4e9be73029a772656b
djalal <djalal@users.noreply.github.com>
2022-07-04 19:37:13 +0200

    Update build.sh

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -13,4 +13,9 @@ su www-data -s /bin/sh -c ' composer install --no-cache --optimize-autoloader --
 
 chmod 755 /var/www/html/pim-community-standard/bin/console
 
+
+echo "replace standard docker-entrypoint to manage persistent folders in custom entrypoint"
+cp /.artifakt/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+chmod +x /usr/local/bin/docker-entrypoint.sh
+
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "

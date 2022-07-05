b5e6abac654aa5b62204e57231754ba2f58337b4
djalal <djalal@users.noreply.github.com>
2022-07-05 12:40:18 +0200

    fix startup script (#28)
    * Create docker-entrypoint.sh
    
    * Update build.sh
    
    * preserve env. vars w/ su
    
    * Update entrypoint.sh
    
    * reset-indexes
    
    * force create session table

---
+++ .artifakt/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+PERSISTENT_FOLDER_LIST=("public/media" "public/uploads" "var")
+
+for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
+  echo Mount $persistent_folder directory
+  rm -rf /var/www/html/pim-community-standard/$persistent_folder && \
+    mkdir -p /data/$persistent_folder && \
+    ln -sfn /data/$persistent_folder /var/www/html/pim-community-standard/$persistent_folder && \
+    chown -h www-data:www-data /var/www/html/pim-community-standard/$persistent_folder /data/$persistent_folder
+done
+
+echo Mount "var/logs" directory
+rm -rf /var/www/html/pim-community-standard/var/logs && \
+  mkdir -p /var/log/artifakt && \
+  ln -sfn /var/log/artifakt /var/www/html/pim-community-standard/var/logs && \
+  chown -h www-data:www-data /var/www/html/pim-community-standard/var/logs /var/log/artifakt
+
+if [[ -x "/.artifakt/entrypoint.sh" ]]; then
+  if [ $ARTIFAKT_IS_MAIN_INSTANCE == 1 ]; then
+    source /.artifakt/entrypoint.sh
+  fi
+fi
+  
+#su www-data -s /bin/sh -c './bin/console pim:installer:check-requirements'
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
+fi
+
+exec "$@"
--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -12,6 +12,13 @@ export APP_DATABASE_PASSWORD=${ARTIFAKT_MYSQL_PASSWORD:-changeme}
 export APP_DATABASE_HOST=${ARTIFAKT_MYSQL_HOST:-mysql}
 export APP_DATABASE_PORT=${ARTIFAKT_MYSQL_PORT:-3306}
 
+echo "APP_DATABASE_NAME=${APP_DATABASE_NAME}"         >  /var/www/html/pim-community-standard/.env.local
+echo "APP_DATABASE_USER=${APP_DATABASE_USER}"         >> /var/www/html/pim-community-standard/.env.local
+echo "APP_DATABASE_PASSWORD=${APP_DATABASE_PASSWORD}" >> /var/www/html/pim-community-standard/.env.local
+
+chown www-data:www-data /var/www/html/pim-community-standard/.env.local
+chown -R www-data:www-data /var/www/html/pim-community-standard/config
+
 ES_PROTOCOL=""
 if [[ "$ARTIFAKT_ES_PORT" == "443" ]]; then
   ES_PROTOCOL="https://"
@@ -20,13 +27,13 @@ export APP_INDEX_HOSTS=${ES_PROTOCOL:-http://}${ARTIFAKT_ES_HOST:-elasticsearch}
 
 wait-for ${ARTIFAKT_ES_HOST:-elasticsearch}:${ARTIFAKT_ES_PORT:-9200} --timeout=30
 
-wait-for $APP_DATABASE_HOST:3306 --timeout=90 -- su www-data -s /bin/bash -c '
+wait-for $APP_DATABASE_HOST:3306 --timeout=90 -- su --preserve-environment www-data -s /bin/bash -c '
   cd /var/www/html/pim-community-standard
   ./bin/console pim:system:information 2>/dev/null;
 
   if [ $? -ne 0 ]; then
   	echo FIRST DEPLOYMENT, will run default installer
-    APP_ENV=dev php bin/console pim:installer:db --doNotDropDatabase --withoutIndexes=true --catalog vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal
+    APP_ENV=dev php bin/console pim:installer:db --doNotDropDatabase --catalog vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal    
     php ./bin/console pim:user:create admin password123 user@example.com Admin User en_US --admin -n
   else
   	echo FOUND INSTALLED SYSTEM, will not run installer
